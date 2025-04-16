27605ceb0602158f9c81f94d28aeef94e4e9d4ee
Mihail <misterslender552@gmail.com>
2025-04-16 20:42:12 +0500

    Initial commit

---
+++ src/main/java/tpm/excel/ExcelReader.java
@@ -0,0 +1,4 @@
+package tpm.excel;
+
+public class ExcelReader {
+}
---
+++ src/main/java/tpm/excel/ItemField.java
@@ -0,0 +1,13 @@
+package tpm.excel;
+
+public class Fields {
+    private long barcode;
+    private String productName;
+    private String description;
+    private String nutrition;
+    private String brand;
+    private int expiration;
+    private String storageCondition;
+    private String composition;
+    private String image;
+}
